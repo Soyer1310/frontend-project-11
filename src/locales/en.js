d69347a5142126f0693c62da9f9d1e39c7d5a17d
Soyer1310 <soyer131313@gmail.com>
2022-11-24 20:27:58 +0300

    create locales

---
+++ src/locales/en.js
@@ -0,0 +1,17 @@
+export default {
+  translation: {
+    header: 'RSS aggregator',
+    introdution: "Start reading RSS today! It's easy, it's beautiful.",
+    placeholder: 'RSS link',
+    example: 'Example: https://ru.hexlet.io/lessons.rss',
+    button: 'Add',
+    successful_message: 'RSS successfully uploaded',
+    error_messages: {
+      incorrect_format: 'The link must be a valid URL',
+      duble_link: 'RSS already exists',
+      field_invalid: 'Feild is invalid',
+      incorrect_resource: 'Resource does not contain valid RSS',
+      required_feild: 'This is a required field',
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,17 @@
+export default {
+  translation: {
+    header: 'RSS агрегатор',
+    introdution: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    placeholder: 'Ссылка RSS',
+    example: 'Пример: https://ru.hexlet.io/lessons.rss',
+    button: 'Добавить',
+    successful_message: 'RSS успешно загружен',
+    error_messages: {
+      incorrect_format: 'Ссылка должна быть валидным URL',
+      duble_link: 'RSS уже существует',
+      field_invalid: 'Поле не валидно',
+      incorrect_resource: 'Ресурс не содержит валидный RSS',
+      required_feild: 'Требуется заполнить поле',
+    },
+  },
+};
