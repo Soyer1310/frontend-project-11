f8172beccae6074bf34a284b81b4c57b90e47780
Soyer1310 <soyer131313@gmail.com>
2022-11-24 20:27:14 +0300

    add setLocale

--- src/validation.js
+++ src/validation.js
@@ -1,9 +1,19 @@
 import * as yup from 'yup';
 
+yup.setLocale({
+  mixed: {
+    default: 'field_invalid',
+    notOneOf: 'duble_link',
+    required: 'required_feild',
+  },
+  string: {
+    url: 'incorrect_format',
+  },
+});
+
 export default (value, feedList) => {
-  const urlSchema = yup.string().url('Ссылка должна быть валидным URL').required().notOneOf(
+  const urlSchema = yup.string().url().required().notOneOf(
     feedList,
-    'Данный фид уже добавлен в список',
   );
   return urlSchema.validate(value, { abortEarly: false });
 };
