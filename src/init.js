7cea52ab77bb5a06ad5b1da0ef883a68958054ef
Soyer1310 <soyer131313@gmail.com>
2022-11-24 20:24:28 +0300

    add i18next functions

--- src/init.js
+++ src/init.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
 import onChange from 'on-change';
+import i18next from 'i18next';
+import resources from './locales/index.js';
 import render from './render.js';
 import validate from './validation.js';
 
 export default () => {
+  const defaultLanguage = 'ru';
+  const i18nInstance = i18next.createInstance();
+  i18nInstance.init({
+    lng: defaultLanguage,
+    debug: false,
+    resources,
+  });
+
   const state = {
     rssForm: {
       value: 'Ссылка RSS',
@@ -15,29 +25,29 @@ export default () => {
   };
 
   const watchedState = onChange(state, () => {
-    render(state);
+    render(state, i18nInstance);
   });
 
   const form = document.querySelector('form');
   form.addEventListener('submit', (e) => {
     e.preventDefault();
+    watchedState.rssForm.validation = 'valid';
     const formData = new FormData(e.target);
     const urlString = formData.get('url');
     watchedState.rssForm.value = urlString;
-    console.log(urlString);
     const response = axios.get(urlString);
     response.then(((resp) => console.log(resp.data)));
     validate(urlString, watchedState.rssForm.feedList).then(() => {
+      console.log('QQQQQQQQQQQQQQQQQQQQQQQQQQQQ');
       watchedState.rssForm.validation = 'valid';
       watchedState.rssForm.state = 'formSubmited';
       watchedState.rssForm.feedList.push(urlString);
       watchedState.rssForm.errors = [];
     }).catch((error) => {
       watchedState.rssForm.validation = 'invalid';
-      watchedState.rssForm.errors.push(error.value);
-      console.log('$$$', error);
-      console.log(state.rssForm.validation);
+      const messages = error.errors.map((err) => `error_messages.${err}`);
+      watchedState.rssForm.errors = messages;
     });
-    console.log('!!!', state.rssForm.feedList);
   });
+  render(state, i18nInstance);
 };
