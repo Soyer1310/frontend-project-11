2f2ecc089ee2f6edabb075dc39d3e37af3fd81d7
Soyer1310 <soyer131313@gmail.com>
2022-11-26 22:00:22 +0300

    add some strings to locales

--- src/locales/en.js
+++ src/locales/en.js
@@ -6,12 +6,18 @@ export default {
     example: 'Example: https://ru.hexlet.io/lessons.rss',
     button: 'Add',
     successful_message: 'RSS successfully uploaded',
+    posts_titel: 'Posts',
+    feeds_titel: 'Feeds',
+    view: 'View',
     error_messages: {
       incorrect_format: 'The link must be a valid URL',
       duble_link: 'RSS already exists',
       field_invalid: 'Feild is invalid',
       incorrect_resource: 'Resource does not contain valid RSS',
       required_feild: 'This is a required field',
+      network_error: 'Network error',
+      parsing_error: 'Parsing error',
+
     },
   },
 };
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -6,12 +6,17 @@ export default {
     example: 'Пример: https://ru.hexlet.io/lessons.rss',
     button: 'Добавить',
     successful_message: 'RSS успешно загружен',
+    posts_titel: 'Посты',
+    feeds_titel: 'Фиды',
+    view: 'Просмотр',
     error_messages: {
       incorrect_format: 'Ссылка должна быть валидным URL',
       duble_link: 'RSS уже существует',
       field_invalid: 'Поле не валидно',
       incorrect_resource: 'Ресурс не содержит валидный RSS',
-      required_feild: 'Требуется заполнить поле',
+      required_feild: 'Поле не должно быть пустым',
+      network_error: 'Ошибка сети',
+      parsing_error: 'Ошибка парсинга RSS-файла',
     },
   },
 };
