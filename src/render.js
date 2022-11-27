fccf81159c65f3b45abf928202adabba90380a28
Soyer1310 <soyer131313@gmail.com>
2022-11-27 22:01:02 +0300

    move postsRender func to separete module

--- src/render.js
+++ src/render.js
@@ -1,50 +1,10 @@
+import buildCard from './buildCard.js';
+import postsRender from './postsRender.js';
+
 const input = document.getElementById('url-input');
 const messagesElem = document.querySelector('.feedback');
-const postsContainer = document.querySelector('div.posts');
 const feedsContainer = document.querySelector('div.feeds');
 
-const buildCard = (i18nInstance, title) => {
-  const card = document.createElement('div');
-  card.classList.add('card', 'border-0');
-  const cardBody = document.createElement('div');
-  cardBody.classList.add('cart-body');
-  card.append(cardBody);
-  const cardTitle = document.createElement('h2');
-  cardTitle.classList.add('card-title', 'h4');
-  cardTitle.textContent = i18nInstance.t(title);
-  cardBody.append(cardTitle);
-  const ul = document.createElement('ul');
-  ul.classList.add('list-group', 'border-0', 'rounded-0');
-  card.append(ul);
-  return card;
-};
-
-const buildPostsList = (state, i18nInstance) => {
-  postsContainer.innerHTML = '';
-  const card = buildCard(i18nInstance, 'posts_titel');
-  const ul = card.querySelector('ul');
-  state.posts.forEach((post) => {
-    const li = document.createElement('li');
-    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
-    const link = document.createElement('a');
-    link.classList.add('fw-bold');
-    link.setAttribute('href', post.link);
-    link.setAttribute('target', 'blank');
-    link.dataset.id = post.id;
-    link.textContent = post.title;
-    li.append(link);
-    const button = document.createElement('button');
-    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-    button.setAttribute('type', 'button');
-    button.dataset.id = post.id;
-    button.dataset.bsToggle = 'model';
-    button.dataset.bsTarget = '#model';
-    button.textContent = i18nInstance.t('view');
-    li.append(button);
-    ul.append(li);
-  });
-  postsContainer.append(card);
-};
 const buildFeedsList = (state, i18nInstance) => {
   feedsContainer.innerHTML = '';
   const card = buildCard(i18nInstance, 'feeds_titel');
@@ -85,7 +45,6 @@ export default (state, i18nInstance) => {
     input.classList.add('is-invalid');
     input.value = state.rssForm.value;
     messagesElem.textContent = state.rssForm.errors.map((err) => i18nInstance.t(err)).join('\n');
-    console.log(messagesElem);
     input.blur();
   }
   if (state.rssForm.validation === 'valid') {
@@ -95,7 +54,7 @@ export default (state, i18nInstance) => {
     messagesElem.classList.remove('text-danger');
     messagesElem.classList.add('text-success');
     messagesElem.textContent = i18nInstance.t('successful_message');
-    buildPostsList(state, i18nInstance);
+    postsRender(state, i18nInstance);
     buildFeedsList(state, i18nInstance);
     input.value = '';
     input.focus();
