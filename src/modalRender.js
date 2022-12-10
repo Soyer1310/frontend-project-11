c9ff17bfab038b4eb6ca6c31782be730c31d4df0
Soyer1310 <soyer131313@gmail.com>
2022-12-10 20:49:49 +0300

    fix modal rendering by removing visible/hidden

--- src/modalRender.js
+++ src/modalRender.js
@@ -1,15 +1,15 @@
-import _ from 'lodash';
 import { Modal } from 'bootstrap';
 
-export default (state) => {
+export default (state, visitedPosts) => {
   const myModal = new Modal(document.getElementById('modal'), {
     keyboard: false,
   });
+  const currentPostID = visitedPosts[visitedPosts.length - 1];
+  console.log(currentPostID);
   myModal.show();
   const modalTitle = document.querySelector('.modal-title');
   const modalDescription = document.querySelector('.modal-body');
   const modalLink = document.querySelector('.full-article');
-  const currentPostID = _.find(state.uiPosts, (uiPost) => uiPost.visibility === 'visible').postId;
   state.posts.forEach((post) => {
     if (post.postId === currentPostID) {
       modalTitle.textContent = post.postTitle;
