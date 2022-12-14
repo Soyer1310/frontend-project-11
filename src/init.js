4a8c3d6dccc294a19c4b6467b162282ef2533f34
Soyer1310 <soyer131313@gmail.com>
2022-12-14 22:28:15 +0300

    fix updater

--- src/init.js
+++ src/init.js
@@ -1,11 +1,32 @@
 import axios from 'axios';
+import * as yup from 'yup';
 import i18next from 'i18next';
 import _ from 'lodash';
 import render from './render.js';
 import resources from './locales/index.js';
-import validate from './validation.js';
 import XMLparser from './parser.js';
 import watcher from './watcher.js';
+
+const getFeedsLinks = (feeds) => feeds.map((feed) => feed.feedLink);
+
+yup.setLocale({
+  mixed: {
+    default: 'field_invalid',
+    notOneOf: 'duble_link',
+    required: 'required_feild',
+  },
+  string: {
+    url: 'incorrect_format',
+  },
+});
+
+const validate = (value, feedList) => {
+  const links = getFeedsLinks(feedList);
+  const urlSchema = yup.string().url().required().notOneOf(
+    links,
+  );
+  return urlSchema.validate(value, { abortEarly: false });
+};
 
 const getRSScontent = (url) => {
   const preparedURL = new URL('https://allorigins.hexlet.app/get');
@@ -24,8 +45,6 @@ export default () => {
   }).then(() => {
     const state = {
       rssForm: {
-        value: 'Ссылка RSS',
-        feedList: [],
         state: 'formFilling',
         errors: [],
         validation: 'valid',
@@ -48,49 +67,45 @@ export default () => {
       }
     };
     const updater = () => {
-      setTimeout(() => {
-        if (watchedState.posts.length > 0) {
-          const links = state.rssForm.feedList;
-          const requests = links.map((link) => getRSScontent(link));
-          const requestsPromise = Promise.all(requests);
-          requestsPromise.then((contents) => contents.forEach((content) => {
-            const parsedRSS = XMLparser(content.data.contents);
-            const { posts } = parsedRSS;
-            const unionPosts = _.unionWith(watchedState.posts, posts, comparePosts);
-            watchedState.posts = unionPosts;
-          }))
-            .finally(() => {
-              updater(state);
-            });
-        } else {
-          updater(state);
-        }
-      }, 5000);
+      const links = getFeedsLinks(state.feeds);
+      const requests = links.map((link) => getRSScontent(link));
+      const requestsPromise = Promise.all(requests);
+      requestsPromise.then((contents) => contents.forEach((content) => {
+        const parsedRSS = XMLparser(content.data.contents);
+        const { posts } = parsedRSS;
+        const unionPosts = _.unionWith(watchedState.posts, posts, comparePosts);
+        watchedState.posts = unionPosts;
+      }))
+        .finally(() => {
+          setTimeout(updater, 5000);
+        });
     };
     const form = document.querySelector('form');
     form.addEventListener('submit', (e) => {
       e.preventDefault();
       watchedState.rssForm.state = 'formFilling';
+      watchedState.rssForm.errors = [];
       const formData = new FormData(e.target);
       const urlString = formData.get('url');
-      watchedState.rssForm.value = urlString;
-      validate(urlString, watchedState.rssForm.feedList).then(() => {
+      validate(urlString, watchedState.feeds).then(() => {
         watchedState.rssForm.validation = 'valid';
         getRSScontent(urlString)
           .catch(() => {
             watchedState.rssForm.errors = ['error_messages.network_error'];
           })
-          .then(((resp) => XMLparser(resp.data.contents)))
+          .then(((resp) => XMLparser(resp.data.contents, urlString)))
           .then((parsedRSS) => {
-            const { feedTitle, feedDescription, feedID } = parsedRSS;
-            const feed = { feedTitle, feedDescription, feedID };
+            const {
+              feedTitle, feedDescription, feedLink, feedID,
+            } = parsedRSS;
+            const feed = {
+              feedTitle, feedDescription, feedLink, feedID,
+            };
             watchedState.feeds.push(feed);
             const { posts } = parsedRSS;
             const unionPosts = _.unionWith(watchedState.posts, posts, comparePosts);
             watchedState.posts = unionPosts;
             watchedState.rssForm.state = 'formSubmited';
-            watchedState.rssForm.feedList.push(urlString);
-            watchedState.rssForm.errors = [];
           })
           .catch(() => {
             watchedState.rssForm.validation = 'invalid';
