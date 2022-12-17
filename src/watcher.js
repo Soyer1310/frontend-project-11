b14fa7bf7f07e6d0cb2a91b7f25a861a582d2576
Soyer1310 <soyer131313@gmail.com>
2022-12-17 10:23:43 +0300

    add elements argument to watcher

--- src/watcher.js
+++ src/watcher.js
@@ -4,15 +4,15 @@ import modalRender from './modalRender.js';
 import visitRender from './visitRender.js';
 import render from './render.js';
 
-export default (state, i18nInstance) => {
+export default (state, i18nInstance, elements) => {
   const watchedState = onChange(state, (path, value) => {
     if (path === 'posts') {
-      postsRender(state, i18nInstance);
+      postsRender(state, i18nInstance, elements);
     } else if (path === 'visitedPosts') {
-      modalRender(state, value);
+      modalRender(state, value, elements);
       visitRender(state.visitedPosts);
     } else {
-      render(state, i18nInstance);
+      render(state, i18nInstance, render);
     }
   });
   return watchedState;
