c8c3a8ce743b90edb58f85fb4c3cffce10968fbf
Soyer1310 <soyer131313@gmail.com>
2023-01-07 11:41:34 +0300

    fix finding of post

--- src/renderModal.js
+++ src/renderModal.js
@@ -3,8 +3,8 @@ export default (state, elements) => {
   const { modalTitle } = elements;
   const { modalDescription } = elements;
   const { modalLink } = elements;
-  const currentPost = state.posts.filter((post) => post.id === currentPostID);
-  modalTitle.textContent = currentPost[0].title;
-  modalDescription.textContent = currentPost[0].description;
-  modalLink.href = currentPost[0].url;
+  const currentPost = state.posts.find((post) => post.id === currentPostID);
+  modalTitle.textContent = currentPost.title;
+  modalDescription.textContent = currentPost.description;
+  modalLink.href = currentPost.link;
 };
